0035eef2d7f5ec85658758e80df5cb8d135c2a56
KING BAYBARS <kingbaybars.yo1@gmail.com>
2026-02-20 11:05:48 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,6 +14,7 @@ import { useCategories } from '@/components/providers/CategoryProvider';
 import { useFirestore, useDoc, useMemoFirebase } from '@/firebase';
 import { doc } from 'firebase/firestore';
 import { useToast } from '@/hooks/use-toast';
+import { cn } from '@/lib/utils';
 
 function HomeContent() {
   const [searchTerm, setSearchTerm] = useState('');
